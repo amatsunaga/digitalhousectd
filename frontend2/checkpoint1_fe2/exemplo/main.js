04ebdd1b204539228e8857f026363dd81eaa14f0
Andrea <andreamizuno@gmail.com>
2022-03-23 17:48:47 -0300

    update checkpoint1 fe2

---
+++ frontend2/checkpoint1_fe2/exemplo/main.js
@@ -0,0 +1,22 @@
+let nomeUsuarioReferencia = document.querySelector ('#nomeUsuario')
+let 
+
+
+
+let usuarios = []
+
+for (let usuario of usuarios) {
+  fjoeirh += ``
+}
+
+
+botaoLoginReferencia.addEventListener('click', function(event) {
+  event.preventDefault()
+
+  let usuario = {
+    nome: nomeUsuarioReferencia.value,
+    senha: senhaUsuarioReferencia.value
+  }
+})
+
+console.log(usuario)
