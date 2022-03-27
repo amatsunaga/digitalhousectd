a70a8cb6860d72321aa752439b252721d4b5ee36
Andrea <andreamizuno@gmail.com>
2022-03-26 23:55:25 -0300

    update aulas 4, 5 e 6 fe2

---
+++ frontend2/aula4/exemplo/script.js
@@ -0,0 +1,4 @@
+var tituloPrincipal = document.querySelector("#tituloPrincipal");
+
+tituloPrincipal.style.color = "green";
+tituloPrincipal.style.fontSize = "3rem";
---
+++ frontend2/aula5e6/exemplo/main.js
@@ -0,0 +1,5 @@
+const mainMenuReference = document.querySelector("#mainMenu");
+
+function handleMenu() {
+  mainMenuReference.classList.toggle("main-menu-open");
+}
